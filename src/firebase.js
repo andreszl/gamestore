4ce2dc127f01b6210dfe7ca8de58c1e086876462
Full stack developer <andreszl.devcode@gmail.com>
2018-05-14 16:19:37 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyBjVeHMSgNrNgVBQ0v4P89Y8JZrCWA70bQ",
-    authDomain: "gamestore-c258a.firebaseapp.com",
-    databaseURL: "https://gamestore-c258a.firebaseio.com",
-    projectId: "gamestore-c258a",
+  apiKey: "",
+    authDomain: "g",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "718426473014"
+    messagingSenderId: ""
 }
 
 export const firebaseApp = firebase.initializeApp(config)
